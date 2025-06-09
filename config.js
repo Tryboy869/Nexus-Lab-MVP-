8917b5a9504ced1ff1e55673ccd792b19c3126a5
Tryboy869 <anzizdaouda0@gmail.com>
2025-06-09 07:56:22 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY";
+const GITHUB_TOKEN = "YOUR_GITHUB_PERSONAL_ACCESS_TOKEN";
+
+// NOTE: For security, do not commit this file if you make it public.
